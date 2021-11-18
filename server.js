b1856fd2e5a359802b1373150dcc58d8cef296a4
Alexandr Rebikov <rebikovlk@mail.ru>
2021-11-18 16:36:41 +0700

    h

--- server.js
+++ server.js
@@ -1,36 +1,42 @@
 const Koa = require('koa');
 const app = new Koa();
+const port = process.env.PORT || 3333;
 
-const ticket = {
-    id: 'идентификатор (уникальный в пределах системы)',
-    name: 'краткое описание',
-    status: 'boolean - сделано или нет',
-    created: 'дата создания (timestamp)'
-}
-
-const ticketFull = {
-    id: 'идентификатор (уникальный в пределах системы)',
-    name: 'краткое описание',
-    description: 'полное описание',
-    status: 'boolean - сделано или нет',
-    created: 'дата создания (timestamp)'
-}
-
-const tickets = [ticket, ticketFull];
-
-app.use(async ctx => {
-    ctx.body = 'Hello ddd World';
-    const { method } = ctx.request.querystring;
-    console.log(method);
-    switch (method) {
-        case 'allTickets':
-            ctx.response.body = tickets;
-            return;
-        // TODO: обработка остальных методов
-        default:
-            ctx.response.status = 404;
-            return;
-    }
+app.use(ctx => {
+    ctx.body = 'Hello Koa';
 });
 
-app.listen(3333);
+
+// const ticket = {
+//     id: 'идентификатор (уникальный в пределах системы)',
+//     name: 'краткое описание',
+//     status: 'boolean - сделано или нет',
+//     created: 'дата создания (timestamp)'
+// }
+
+// const ticketFull = {
+//     id: 'идентификатор (уникальный в пределах системы)',
+//     name: 'краткое описание',
+//     description: 'полное описание',
+//     status: 'boolean - сделано или нет',
+//     created: 'дата создания (timestamp)'
+// }
+
+// const tickets = [ticket, ticketFull];
+
+// app.use(async ctx => {
+//     ctx.body = 'Hello ddd World';
+//     const { method } = ctx.request.querystring;
+//     console.log(method);
+//     switch (method) {
+//         case 'allTickets':
+//             ctx.response.body = tickets;
+//             return;
+//         // TODO: обработка остальных методов
+//         default:
+//             ctx.response.status = 404;
+//             return;
+//     }
+// });
+
+app.listen(port);
