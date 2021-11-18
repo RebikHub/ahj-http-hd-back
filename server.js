2180c1ad5d41439200e4f64a5c3e0086e17c3f0c
Alexandr Rebikov <rebikovlk@mail.ru>
2021-11-18 10:25:01 +0700

    koa test

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
-const port = process.env.PORT || 7070;
-const server = http.createServer(app.callback()).listen(port);
+// const port = process.env.PORT || 7070;
+// const server = http.createServer(app.callback()).listen(port);
+
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(3000);
