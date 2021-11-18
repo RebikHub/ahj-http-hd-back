4d0fdeaecdf42d9b70d49e8dde1c25754fcc06d8
Alexandr Rebikov <rebikovlk@mail.ru>
2021-11-18 15:17:26 +0700

    not work forever

--- server.js
+++ server.js
@@ -1,32 +1,27 @@
 const Koa = require('koa');
 const app = new Koa();
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
-});
-
-app.listen(3333);
-
 const ticket = {
-    id, // идентификатор (уникальный в пределах системы)
-    name, // краткое описание
-    status, // boolean - сделано или нет
-    created, // дата создания (timestamp)
+    id: 'идентификатор (уникальный в пределах системы)',
+    name: 'краткое описание',
+    status: 'boolean - сделано или нет',
+    created: 'дата создания (timestamp)'
 }
 
 const ticketFull = {
-    id, // идентификатор (уникальный в пределах системы)
-    name, // краткое описание
-    description, // полное описание
-    status, // boolean - сделано или нет
-    created, // дата создания (timestamp)
+    id: 'идентификатор (уникальный в пределах системы)',
+    name: 'краткое описание',
+    description: 'полное описание',
+    status: 'boolean - сделано или нет',
+    created: 'дата создания (timestamp)'
 }
 
 const tickets = [ticket, ticketFull];
 
 app.use(async ctx => {
+    ctx.body = 'Hello ddd World';
     const { method } = ctx.request.querystring;
-
+    console.log(method);
     switch (method) {
         case 'allTickets':
             ctx.response.body = tickets;
@@ -37,3 +32,5 @@ app.use(async ctx => {
             return;
     }
 });
+
+app.listen(3333);
