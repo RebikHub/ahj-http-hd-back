0523dd7313511c31454c18e153fc8aa8ba16ed3e
Alexandr Rebikov <rebikovlk@mail.ru>
2021-12-06 16:15:30 +0700

    cors update

--- server.js
+++ server.js
@@ -1,35 +1,21 @@
 const data = require('./tickets');
 const Koa = require('koa');
 const koaBody = require('koa-body');
+const cors = require('koa2-cors');
 const app = new Koa();
 const { v4: uuidv4 } = require('uuid');
 const port = process.env.PORT || 3333;
 
 app.use(koaBody({ urlencoded:true, }));
 
-app.use(async (ctx, next) => {
-    const origin = ctx.request.get('Origin');
-    if (!origin) {
-        return await next();
-    }
-    const headers = {'Access-Control-Allow-Origin':'*',};
-    if (ctx.request.method!=='OPTIONS') {
-        ctx.response.set({...headers});
-        try {
-            return await next();
-        } catch (e) {
-            e.headers = {...e.headers, ...headers};
-            throw e;
-        }
-    }
-    if (ctx.request.get('Access-Control-Request-Method')) {
-        ctx.response.set({...headers,'Access-Control-Allow-Methods':'GET, POST, PUT, DELETE, PATCH',});
-        if (ctx.request.get('Access-Control-Request-Headers')) {
-            ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Allow-Request-Headers'));
-        }
-        ctx.response.status = 204;// No content
-    }
-});
+app.use(
+    cors({
+        origin: '*',
+        credentials: true,
+        'Access-Control-Allow-Origin': true,
+        allowMethods: ['GET', 'POST', 'PUT', 'DELETE'],
+    })
+)
 
 app.use(async ctx => {
     const { method, id } = ctx.request.query;
@@ -121,6 +107,32 @@ app.use(async ctx => {
     ctx.response.status = 404;
     return;
 });
+
+// app.use(async (ctx) => {
+//     let method;
+//     if (ctx.request.method === 'GET') ({ method } = ctx.request.query);
+//     else if (ctx.request.method === 'POST') ({ method } = ctx.request.body);
+  
+//       ctx.response.status = 200;
+      
+//     switch (method) {
+//       case 'allTickets': ctx.response.body = ticketController.getTickets();
+//         break;
+//       case 'ticketById': ctx.response.body = ticketController.getTicketById(ctx.request.query);
+//         break;
+//       case 'createTicket': ctx.response.body = ticketController.createTicket(ctx.request.body);
+//         break;
+//       case 'changeStatus': ctx.response.body = ticketController.changeStatus(ctx.request.body);
+//         break;
+//       case 'updateTicket': ctx.response.body = ticketController.updateTicket(ctx.request.body);
+//         break;
+//       case 'deleteTicket': ctx.response.body = ticketController.deleteTicket(ctx.request.body);
+//         break;
+//       default:
+//         ctx.response.status = 400;
+//         ctx.response.body = `Unknown method '${method}' in request parameters`;
+//     }
+//   });
 
 
 app.listen(port, () => console.log('Server started'));
